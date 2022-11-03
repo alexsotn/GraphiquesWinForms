dc4c495ac4813963e7fe0ad972d536adb68a8faa
Alex Sotnik <64767365+alexsotn@users.noreply.github.com>
2022-11-03 19:55:59 +0300

    Add files via upload

---
+++ Kurs_sotn_pen/graphics/Point3D.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+	namespace Kurs_sotn_pen
+{
+    [Serializable]
+    public class Point3D
+    {
+        /// <summary>
+        /// Decart coordinates
+        /// </summary>
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+        public double Length { get { return (float)Math.Sqrt(X * X + Y * Y + Z * Z); } }
+
+        public Point3D(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public Point3D(Point3D point)
+        {
+            X = point.X;
+            Y = point.Y;
+            Z = point.Z;
+        }
+
+        public Point3D(Vector3D vector)
+        {
+            X = vector.X;
+            Y = vector.Y;
+            Z = vector.Z;
+        }
+
+        public virtual void Add(double x, double y, double z)
+        {
+            X += x;
+            Y += y;
+            Z += z;
+        }
+
+        public virtual void Add(Point3D point)
+        {
+            X += point.X;
+            Y += point.Y;
+            Z += point.Z;
+        }
+
+        public virtual void Set(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public Matrix GetProjectiveCoordinates()
+        {
+            double[] result = new double[4];
+            result[0] = X;
+            result[1] = Y;
+            result[2] = Z;
+            result[3] = 1.0;
+            return new Matrix(result);
+        }
+
+        public Point3D Clone()
+        {
+            return new Point3D(this);
+        }
+    }
+}
---
+++ Kurs_sotn_pen/graphics/Point3DSpherical.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+	namespace Kurs_sotn_pen
+{
+    [Serializable]
+    public class Point3DSpherical : Point3D
+    {
+        public double Radius { get; set; } // polar radius (Sphere.radius)
+        public double Theta { get; private set; } // bend angle
+
+        public double Phi { get; private set; } // rotation angle
+
+        public void SetTheta(double value)
+        {
+            Theta = Math.Max(value, 0);
+            UpdateDecart();
+        }
+
+        public void SetPhi(double value)
+        {
+            Phi = value;
+            UpdateDecart(false);
+        }
+
+        public Point3DSpherical(double radius, double theta, double phi) : base(0, 0, 0)
+        {
+            Radius = radius;
+            Theta = Math.Max(Theta, 0);
+            Phi = phi;
+            UpdateDecart();
+        }
+
+        /// <summary>
+        /// Updating Decart coordinates
+        /// </summary>
+        /// <param name="updateY"></param>
+        private void UpdateDecart(bool updateY = true)
+        {
+            X = Radius * Math.Sin(Theta) * Math.Cos(Phi);
+            if (updateY)
+            {
+                Y = Radius * Math.Cos(Theta);
+            }
+            Z = Radius * Math.Sin(Theta) * Math.Sin(Phi);
+        }
+
+        /// <summary>
+        /// Updating "Spherical coordinates"
+        /// </summary>
+        private void UpdateSpherical()
+        {
+            Radius = Math.Sqrt(X * X + Y * Y + Z * Z);
+            Theta = Math.Max(Math.Acos(Y / Radius), 0);
+            Phi = Math.Atan(Z / X);
+        }
+
+        public Point3DSpherical(Point3D point) : base(point)
+        {
+            Set(point.X, point.Y, point.Z);
+        }
+
+        public override void Add(double dx, double dy, double dz)
+        {
+            base.Add(dx, dy, dz);
+            UpdateSpherical();
+        }
+
+        public virtual void Set(Point3D point)
+        {
+            Set(point.X, point.Y, point.Z);
+        }
+
+        public override void Set(double x, double y, double z)
+        {
+            base.Set(x, y, z);
+            UpdateSpherical();
+            if (X < 0)
+            {
+                Phi = Math.PI + Phi;
+            }
+            else if (Z < 0)
+            {
+                Phi = 2 * Math.PI + Phi;
+            }
+        }
+    }
+}
